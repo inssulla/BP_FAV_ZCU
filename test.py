ab1090a44c0056ffabd3ca669f8753833c5eb21b
Yauheni Varabyou <93829132+inssula@users.noreply.github.com>
2022-09-27 20:04:20 +0200

    Add test file to Git for training

---
+++ test.py
@@ -0,0 +1,15 @@
+print('Hello PyCharm')
+
+print('This is test file, if you understand that write the answer')
+
+while True:
+    answer = input('Write "Yes" or "No": ')
+    if answer == "Yes":
+        print('Great!')
+        break
+    elif answer == "No":
+        print('Try one more time..\n')
+        continue
+    else:
+        print('You have just 2 options...\n')
+        continue
